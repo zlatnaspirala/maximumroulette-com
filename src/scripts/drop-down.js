eaff27f3ff69518bbe509487dece9fc1852a2187
Nikola <zlatnaspirala@gmail.com>
2018-12-19 00:06:35 +0100

    base for nav

--- src/scripts/drop-down.js
+++ src/scripts/drop-down.js
@@ -1,22 +1,24 @@
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    hideDropDown();
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
 }
 
-function showDropDown(id) {
-  hideDropDown();
-  document.getElementById(id).classList.toggle("show")
-}
+function loadPage(name) {
 
-function hideDropDown() {
-  var dropdowns = document.getElementsByClassName("dropdown-content");
-  var i;
-  for (i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-      openDropdown.classList.remove('show');
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      // Typical action to be performed when the document is ready:
+      document.getElementById("main").innerHTML = xhttp.responseText;
     }
-  }
+  };
+  xhttp.open("GET", "pages/" + name, true);
+  xhttp.send();
+
 }
+
